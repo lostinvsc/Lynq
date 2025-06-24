d55cdd8aaa8effc62ba3891436d416037ce36910
sachin <sachinnagar2004@gmail.com>
2025-06-24 14:38:03 +0530

    file structuring

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Lynq",
+  description: "Lynq is a privacy-first URL shortener that lets users create short links with built-in expiry control, click tracking, CAPTCHA protection, and user authentication. Your links, your control — locked in a vault.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
